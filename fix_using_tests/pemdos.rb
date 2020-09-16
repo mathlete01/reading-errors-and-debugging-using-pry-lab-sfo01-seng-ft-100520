e19bb458b7c71d968ed5eddfd91a3f312e9891c8
Matthew Sallin <msallin@gmail.com>
2020-09-16 00:17:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,12 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  binding.pry
   if string[0] == "s"
+    foo = 10 * "s"
     10 * "s" + string
+    binding.pry
   else
     string
   end
